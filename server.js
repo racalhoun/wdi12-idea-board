dcf82b7155ec17314062c45fac501ec3165c7c25
racalhoun <racalhoun489@gmail.com>
2017-10-12 11:19:41 -0400

    added concurrentlt

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/', (req, res)=>{
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log('App is listening', PORT)
 })
